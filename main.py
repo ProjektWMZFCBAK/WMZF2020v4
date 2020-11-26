b4db3a2a6f6ea1d24c930dc46537d2388af2e145
Aleksander Kaczorowski <a.kaczorowski2312@wp.pl>
2020-11-26 23:52:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import math
+import matplotlib.pyplot as plt
+import numpy
+#użytkownik wpisuje wymagane dane do działania programu
+#bedzie tez tu system pilnowania, aby uzytkownik wpisywal dane odpowiedniego typu
+print('Proszę podać dane')
+c = input() #wielka polos elipsy
+a = input() #odcinek miedzy srodkiem a jednym z ognisk elipsy
+S = input() #pole powierzchni zakreslane przez promien wodzacy
+t = input() #czas zakreslania pola powierzchni
+r = input() #odleglosc slonca od planet
+w = input() #predkosc katowa obrotu linii laczacej slonce z planeta
+R1 = input() #polos wielka planety 1
+R2 = input() #polos wielka planety 2
+T1 = input() #okres obiegu planety 1
+T2 = input() #okres obiegu planety 2
+
+
+#program pobiera zdefiniowane funkcje 1,2 i 3 prawa keplera
+
+def prawo_1(): # e = c/a
+    return prawo_1()
+
+def prawo_2(): # (dS)/(dT)=(1/2)(r^2)w
+    return prawo_2()
+
+def prawo_3(): # (R1^3)/(R2^3) = (T1^3)/(T2^3)
+    return prawo_3()
+
+#w tej częsci program wylicza orbite w funkcji 'orbita' na podstawie wpisanych danych wstawionych do trzech zdefiniowanych funkcji praw keplera
+
+def orbita():
+    return orbita()
+
+
+#rysowanie wykresu
+plt.title('Orbita obiektu')
